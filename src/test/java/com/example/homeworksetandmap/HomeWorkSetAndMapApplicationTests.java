c07efeef3e7ad29633b3cd0e73b28832ef924ed1
aires <airesheva@mail.ru>
2023-06-11 14:33:41 +0300

    1 commit

---
+++ src/test/java/com/example/homeworksetandmap/HomeWorkSetAndMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworksetandmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSetAndMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
